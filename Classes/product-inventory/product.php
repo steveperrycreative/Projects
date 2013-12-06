4216b575474ce7e971ba857b4d817a80f63df5fd
Steve Perry <ideas@steveperrycreative.com>
2013-12-06 09:01:28 +0000

    Set up initial classes

--- Classes/product-inventory/product.php
+++ Classes/product-inventory/product.php
@@ -1,13 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: steve
- * Date: 03/12/2013
- * Time: 13:53
- */
 
 class product {
     public $var = price;
     public $var = id;
     public $var = quantity;
+}
+
+class inventory {
+
 }
