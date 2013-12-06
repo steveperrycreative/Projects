5988ddef607ad4b243b078b3038e2377b7210d81
Steve Perry <ideas@steveperrycreative.com>
2013-12-06 08:56:30 +0000

    Ignoring IDE files and added initial project

---
+++ Classes/product-inventory/product.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steve
+ * Date: 03/12/2013
+ * Time: 13:53
+ */
+
+class product {
+    public $var = price;
+    public $var = id;
+    public $var = quantity;
+}
