593544908cd04b76e7c28796d90f867dc809552a
Steve Perry <ideas@steveperrycreative.com>
2013-12-12 11:00:37 +0000

    Playing with Product class

--- Classes/product-inventory/product.php
+++ Classes/product-inventory/product.php
@@ -1,11 +1,29 @@
 <?php
 
-class product {
-    public $var = price;
-    public $var = id;
-    public $var = quantity;
+class Product
+{
+
+    private $price = '';
+    private $id = '';
+    private $quantity = '';
+
+    public function priceFree() {
+        $this->price = 0;
+        echo 'The price is FREE';
+    }
+
+    public function price200() {
+        $this->price = 200;
+        echo 'The price is 200';
+    }
+
 }
 
-class inventory {
+$productOne = new Product;
+$productOne->priceFree();
 
-}
+$productTwo = new Product;
+$productTwo->price200();
+
+echo $productOne;
+echo $productTwo;
